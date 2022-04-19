47071122dd1da834c42c25d8fa9a60f0900a9bec
Jose <jtportillo87@gmail.com>
2022-04-20 00:25:32 +0200

    comentarios

--- js/login.js
+++ js/login.js
@@ -1,17 +1,17 @@
-var attempt = 3; // Variable to count number of attempts.
-// Below function Executes on click of login button.
+var attempt = 3; // Variable para contar el número de intentos
+// La funcion se ejecuta cuando hacemos clic en el boton
 function validate(){
 var exampleInputEmail1 = document.getElementById("exampleInputEmail1").value;
 var exampleInputPassword1 = document.getElementById("exampleInputPassword1").value;
 if ( exampleInputEmail1 == "j" && exampleInputPassword1 == "1"){
 alert ("Login successfully");
-window.location = "salas.html"; // Redirecting to other page.
+window.location = "salas.html"; // Redirige a otra página
 return false;
 }
 else{
-attempt --;// Decrementing by one.
+attempt --;
 alert("You have left "+attempt+" attempt;");
-// Disabling fields after 3 attempts.
+// inhabilita a los 3 intentos
 if( attempt == 0){
 document.getElementById("exampleInputEmail1").disabled = true;
 document.getElementById("exampleInputPassword1").disabled = true;
